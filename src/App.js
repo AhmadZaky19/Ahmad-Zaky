e4cd1f97912217893373138a130636e6f92f655a
AhmadZaky19 <a.zaky32@gmail.com>
2022-09-04 18:41:51 +0700

    Setup redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import './App.css';
+import { Provider } from "react-redux";
+import store from "./stores/store";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
